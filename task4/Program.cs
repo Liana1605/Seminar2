54a1cec84b539a2c7ce42400d2612635106e1e61
liana16 <liana_you@bk.ru>
2022-11-29 16:36:38 +0300

    Задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка (10, 99) и показывает наибольшую цифру
+int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+
+int a2 = a % 10;
+int a1 = a / 10;
+int result = Math.Max(a1, a2);
+Console.WriteLine(result);
