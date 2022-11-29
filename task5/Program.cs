dbe01b4f36751fcbe725f3a8245ed64006b29355
liana16 <liana_you@bk.ru>
2022-11-29 16:35:47 +0300

    Задача 5

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выдает случайное трехзначное число и удаляет вторую цифру этого числа
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+
+int a3 = a % 10;
+int a1 = a / 100;
+int result = (a1 * 10 + a3);
+Console.WriteLine(result);
